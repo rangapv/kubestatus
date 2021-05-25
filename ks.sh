0a4a985855500b72659152fed540abf5240ea84c
rangapv <rangapv@yahoo.com>
2021-05-25 16:01:25 +0000

    Added function call

--- ks.sh
+++ ks.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 echo "THis is to inform the kubernetes cluster status in this box"
+
+component() {
+args1="$@"
+pargs="$#"
+echo "args1 is $args1"
+k1=$(ps -ef | grep "$@" | grep -v "grep")
+k1s="$?"
+echo "Inside component"
+echo "k1s is $k1s"
+if [[ ( $k1s = 0 ) ]]
+then
+  ret=0
+else
+  ret=1
+fi
+echo "ret is $ret"
+}
+
+
+
+component kube-scheduler
+echo "klet1 is $klet1"
+if [[ ( $ret = 0 ) ]]
+then
+  echo "componet function for kubelet worked"
+fi
+
 klet=$(ps -ef |grep kubelet)
 klets="$?"
 if [[( $klets = 0 ) ]]
