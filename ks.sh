4f4d254852105f0aff2466cc6ee5f767b5022937
rangapv <rangapv@yahoo.com>
2021-05-26 09:06:51 +0000

    Adding more k8s components status

--- ks.sh
+++ ks.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "THis is to inform the kubernetes cluster status in this box"
-klet=$(ps -ef |grep kubelet)
-klets="$?"
-
 
 component(){
 args1="$@"
@@ -12,7 +9,7 @@ pargs="$#"
 retr=$(ps -ef | grep $args1 | grep -v grep | wc -l)
 if [[ ( $retr = 1 ) ]]
 then
-   echo "$args1 is running on this Box"
+   echo "\"$args1\" is running on this Box"
 fi
 
 }
@@ -22,6 +19,8 @@ component /usr/bin/kubelet
 component kube-apiserver
 component kube-controller-manager
 component kube-scheduler
-component /usr/bin/dockerd 
-#component kubelet
+component etcd 
+component kube-proxy 
+component flanneld 
+component dashboard 
 
