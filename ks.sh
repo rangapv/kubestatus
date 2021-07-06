522c302b7632eb1dee40b71ffe1feba5b068b8f7
rangapv <rangapv@yahoo.com>
2021-07-06 11:26:54 +0000

    Added tables and columns for DISPLAY

--- ks.sh
+++ ks.sh
@@ -51,9 +51,11 @@ do
 done
 if [[ ( $sflag -eq 0) ]]
 then
+	myprint1 Installed-Components
 echo "All these components ${str1::-1} are installed"
 elif [[ ( $nflag -eq 0) ]]
 then
+	myprint1 Missing-Components
 echo "All these components ${str4::-1} are NOT installed"
 fi
 }
@@ -62,15 +64,22 @@ myversion() {
 echo ""
 declare -A arrb
 verarray=("$@")
+myprint1 Component-Version
 for n in ${verarray[@]}
 do
 for h in "${mastera[@]}"
 do
 if [[ ( ${h} = "$n" ) && ( ${arra[$h]} -eq 1 ) ]] 
 then
-	echo "$h is installed in `which $h`"
-	echo "$h version is `$h --version`"
-	echo ""
+#	echo "$h is installed in `which $h`"
+#	echo "$h version is `$h --version`"
+        whch=`which $h`
+	whv=`$h --version`
+	myprint1 $h
+        awk -F: 'BEGIN{printf "%-10s %25s \n", "Install-Path", "Version-Info"
+                       printf "%-10s %25s \n", "------------", "------------"}'
+        awk -F: -v h1="$whch" -v whv="$whv" 'BEGIN{printf "%-10s %25s \n" , h1,  whv}'
+
 fi	   
 done
 done
@@ -87,7 +96,7 @@ cw=$(which $cd)
 cs="$?"
 if [[ $cs -eq 0 ]]
 then
-	echo "This $cd core component is installed in \"$cw\" "
+	awk -v a="$cd" -v b="$cw" 'BEGIN{ printf "%-12s %-15s \n",a, b}'
 	((cc+=1))
 else
 echo "This $cd core component is not installed" 
@@ -95,6 +104,7 @@ fi
 done
 if [[ $cc -eq 0 ]]
 then
+	myprint1 Core-Components-Missing
   echo "None of the core components \"${c[@]}\" are installed "
 fi
 echo ""
@@ -102,19 +112,22 @@ echo ""
 
 myconfig() {
 echo ""
+  myprint1 Core-Components-Configfiles
   arrayc=("$@")
   if [[ ( $nodef = 0 ) ]]
   then
   for m in ${arrayc[@]}
   do
-      echo "$m is using `ps -ef | grep $m | grep "\-\-kubeconfig" | awk '{split($0,a,"--kubeconfig="); print a[2]}' | awk '{split($0,a," "); print a[1]}'`"
+	  compconfig=(`ps -ef | grep $m | grep "\-\-kubeconfig" | awk '{split($0,a,"--kubeconfig="); print a[2]}' | awk '{split($0,a," "); print a[1]}'`)
+         awk -F: -v config="$compconfig" -v m1="$m" 'BEGIN{ printf "%-10s %-25s \n", m1, config }' 
   done
   else
   for m in ${arrayc[@]}
   do
 #  echo "$m is using `ps -ef | grep $m | grep -v grep | grep -v awk | awk '{split($0,a," "); a[8] ~ /kubelet/; print a[10]}' | awk '{split($0,a,"--kubeconfig="); print a[2]}'`"
-  echo "$m is using `ps -ef | grep $m | grep -v grep | grep -v awk | awk '{split($0,a," "); if (a[8]~/kubelet/) for (i=0;i<14;i++) if (a[i]~/--kubeconfig/) print a[i]  a[i+1]}'`"
+compconfig=(`ps -ef | grep $m | grep -v grep | grep -v awk | awk '{split($0,a," "); if (a[8]~/kubelet/) for (i=0;i<14;i++) if (a[i]~/--kubeconfig/) print a[i]  a[i+1]}'`)
   #echo "$m is using `ps -ef | grep $m | grep -v grep | grep -v awk | awk '{split($0,a," "); a[8] ~ /kubelet/; print a[11]}' | awk '{split($0,a,"--kubeconfig="); print a[2]}'`"
+         awk -F: -v config="$compconfig" -v m1="$m" 'BEGIN{ printf "%-10s %-25s \n", m1, config }' 
   done
   fi
 echo ""
@@ -123,7 +136,6 @@ echo ""
 myruntime() {
   arrayr=("$@")
 echo ""
-
   for m in ${arrayr[@]}
   do
 	  rnc=`ps -ef | grep "\-\-container-\runtime\-endpoint" | grep -v grep | wc -l`
@@ -139,6 +151,7 @@ echo ""
 }
 
 myrunc(){
+  myprint1 Container-Runtime
 if [[ $drun -eq 1 ]]
 then
 	echo "This box is using \"Dockerd\" as the runtime"
@@ -150,11 +163,31 @@ fi
 echo ""
 }
 
+myprint1() {
+cl2="$@"
+awk -F: -v c2="$cl2" 'BEGIN{printf "%-12s \n","-------------------"
+                            printf "%-12s \n",c2
+			    printf "%-12s \n", "-----------------"}'
+}
+
+
+myprint() {
+cl1="$@"
+awk -F: -v c1="$cl1" 'BEGIN{printf "%-12s %-15s \n","--------","------------"
+                printf "%-12s %-15s \n","Process","Install Path"
+                printf "%-12s %-15s \n","-------","-------------"}' 
+	}
+
+
+
 core1=( kubeadm kubelet kubectl )
+myprint1 Core-Statistics 
+myprint
 coreinstall "${core1[@]}"
 
 
 mastera=( kubelet kube-apiserver kube-controller-manager kube-scheduler etcd )
+myprint1 Component-Statistics 
 component "${mastera[@]}"
 
 master=$(ps -ef | grep kube | grep -v grep | wc -l)
